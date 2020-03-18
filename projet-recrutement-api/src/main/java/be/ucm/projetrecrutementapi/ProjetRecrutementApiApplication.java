ca465e3e7e3dd89cda37fd4fcb839a71d7699dc3
Comamanel <florent.piette9@gmail.com>
2020-03-18 13:56:16 +0100

    api - initial commit

---
+++ projet-recrutement-api/src/main/java/be/ucm/projetrecrutementapi/ProjetRecrutementApiApplication.java
@@ -0,0 +1,13 @@
+package be.ucm.projetrecrutementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetRecrutementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetRecrutementApiApplication.class, args);
+	}
+
+}
---
+++ projet-recrutement-api/src/test/java/be/ucm/projetrecrutementapi/ProjetRecrutementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucm.projetrecrutementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetRecrutementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
