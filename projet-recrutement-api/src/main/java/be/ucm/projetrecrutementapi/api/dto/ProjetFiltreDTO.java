43e8d94e351f7717bd7b5dae042072532632422d
Comamanel <florent.piette9@gmail.com>
2020-03-25 11:47:19 +0100

    Getters, setters et constructeur sans paramètres ajoutés au ProjetFiltreDTO

--- projet-recrutement-api/src/main/java/be/ucm/projetrecrutementapi/api/dto/ProjetFiltreDTO.java
+++ projet-recrutement-api/src/main/java/be/ucm/projetrecrutementapi/api/dto/ProjetFiltreDTO.java
@@ -2,9 +2,15 @@ package be.ucm.projetrecrutementapi.api.dto;
 
 import be.ucm.projetrecrutementapi.dal.entities.enums.EtatProjet;
 import be.ucm.projetrecrutementapi.dal.entities.enums.TypeProjet;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProjetFiltreDTO {
     private String nom;
     private LocalDate dateDebutEgal;
