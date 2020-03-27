d098f4e93d36541128ddf58d04dbe65c40e482f4
Comamanel <florent.piette9@gmail.com>
2020-03-27 12:07:08 +0100

    Classe vide CandidatureController ajoutée

---
+++ projet-recrutement-api/src/main/java/be/ucm/projetrecrutementapi/api/controllers/CandidatureController.java
@@ -0,0 +1,13 @@
+package be.ucm.projetrecrutementapi.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/candidature")
+public class CandidatureController {
+
+}
